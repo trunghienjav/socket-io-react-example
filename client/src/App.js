d92ec5b5e7669f24b66ad350eaf5444b3d168ca7
minhnt <minhnt@rikkeisoft.com>
2024-01-16 11:28:25 +0900

    done chat basic with RSA feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,57 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
-
+import JSEncrypt from 'jsencrypt';
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   //Room State
   const [room, setRoom] = useState("");
+  const [Joinroom, setJoinRoom] = useState("");
 
   // Messages States
   const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+
+  const [messageReceived, setMessageReceived] = useState([]);
+  // console.log(messageReceived);
+
+  const [publicKey, setPublicKey] = useState(null);
 
   const joinRoom = () => {
     if (room !== "") {
       socket.emit("join_room", room);
+      setJoinRoom(room);
     }
   };
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room });
+
+  socket.on('public-key', (dataKey) => {
+    console.log(dataKey.publicKey)
+    setPublicKey(dataKey.publicKey);
+  });
+
+  const handleSendMessage = () => {
+
+    const encryptor = new JSEncrypt();
+    encryptor.setPublicKey(publicKey);
+    const encryptedMessage = encryptor.encrypt(message);
+    // Check if encryptedMessage is available
+    if (encryptedMessage) {
+      // console.log('encryptedMessage is ' + encryptedMessage);
+      // If encryptedMessage is available, use it as the message
+      socket.emit("send_message", { message: encryptedMessage, room });
+    } else {
+      // If encryptedMessage is not available, use the original message
+      alert("Couldn't send message")
+    }
+    setMessage('')
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
+      setMessageReceived(prevMessages => [...prevMessages, data]);
+      // console.log(data);
+      // setMessageReceived(data);
     });
   }, [socket]);
   return (
@@ -37,14 +64,27 @@ function App() {
       />
       <button onClick={joinRoom}> Join Room</button>
       <input
+        value={message}
         placeholder="Message..."
         onChange={(event) => {
           setMessage(event.target.value);
         }}
+
       />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
+      <button
+        onClick={handleSendMessage}
+      >
+        Send Message
+      </button>
+      <div>
+        <p>Your room: {Joinroom}</p>
+      </div>
+      <h3> Message:</h3>
+      <ul>
+        {messageReceived.map((msg, index) =>
+          <li key={index}>{msg}</li>
+        )}
+      </ul>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const crypto = require('crypto');
 
 app.use(cors());
 
@@ -15,15 +16,40 @@ const io = new Server(server, {
   },
 });
 
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 4096,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem'
+  }
+});
+// console.log('privatekey is:  ' + privateKey);
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  socket.emit('public-key', ({ publicKey: publicKey }));
   socket.on("join_room", (data) => {
     socket.join(data);
   });
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+    // console.log("\n message from client: \n" + data.message);
+    // const decryptedMessage = crypto.privateDecrypt(privateKey, data.message);
+
+    const decryptedMessage = crypto.privateDecrypt({
+      key: privateKey,
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+    }, Buffer.from(data.message, 'base64')).toString('utf8');
+
+    // console.log(decryptedMessage);
+
+    io.in(data.room).emit("receive_message", decryptedMessage);
+    // console.log('\n privateKey is: \n'+ privateKey);
+    console.log('decrypted message is: \n' + decryptedMessage);
   });
 });
 
